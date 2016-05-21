4dcefd83b0d6bcabcbdd56a5dce9496de5a9e83d
Toboshi <evan.welchner@rogers.com>
2016-05-21 10:04:11 -0400

    Added functionality for background elements in the level editor

---
+++ ToyTale/Assets/_Project/Scripts/LevelEditor/EditorBackground.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorBackground : MonoBehaviour
+{
+    [SerializeField]
+    float m_Height = 1;
+
+    bool m_Selected = false;
+    Collider2D collision = null;
+    LevelEditor m_Editor = null;
+
+    void Start()
+    {
+        collision = GetComponent<Collider2D>();
+        m_Editor = FindObjectOfType<LevelEditor>();
+        SetSelected(false);
+    }
+
+    public void Click(Vector3 aPosition, bool aTouchBegan)
+    {
+        if (m_Editor.GetEditMode() == LevelEditor.EditMode.Move)
+        {
+            if (aTouchBegan)
+            {
+                SetSelected(true);
+            }
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            pos.x = Mathf.Round(pos.x / 0.25f) * 0.25f;
+            pos.y = Mathf.Floor(pos.y) - 0.5f;
+            pos.z = transform.position.z;
+            transform.position = pos;
+        }
+        else if (m_Editor.GetEditMode() == LevelEditor.EditMode.Clear)
+        {
+            m_Editor.RemoveBackground(gameObject);
+        }
+    }
+
+    public bool GetSelected()
+    {
+        return m_Selected;
+    }
+
+    public void SetSelected(bool aSelected)
+    {
+        m_Selected = aSelected;
+        if (!m_Selected) return;
+
+        EditorBackground[] backgrounds = FindObjectsOfType<EditorBackground>();
+        foreach (EditorBackground b in backgrounds)
+        {
+            if (b != this) b.SetSelected(false);
+        }
+    }
+
+    //This ALSO needs to go away
+    void OnGUI()
+    {
+        if (!m_Selected) return;
+
+        //Depth
+        float x = Camera.main.WorldToScreenPoint(transform.position).x;
+        float y = Screen.height - Camera.main.WorldToScreenPoint(transform.position + transform.up).y;
+
+        if (GUI.Button(new Rect(x + 20, y - 50 * m_Height, 30, 30), ">")) transform.position += Vector3.forward;
+        if (GUI.Button(new Rect(x - 60, y - 50 * m_Height, 30, 30), "<")) transform.position -= Vector3.forward;
+
+        GUI.Label(new Rect(x - 25, y - 70 * m_Height, 120, 30), "Depth");
+        GUI.Label(new Rect(x - 10, y - 50 * m_Height, 120, 30), transform.position.z.ToString());
+    }
+}
--- ToyTale/Assets/_Project/Scripts/LevelEditor/LevelEditor.cs
+++ ToyTale/Assets/_Project/Scripts/LevelEditor/LevelEditor.cs
@@ -14,11 +14,16 @@ public class LevelEditor : MonoBehaviour
         Clear
     }
 
-    const int SaveGameVersionNum = 0;
+    const int SaveGameVersionNum = 1;
 
     [SerializeField]
     GameObject m_Tile;
     List<GameObject> m_Tiles;
+
+    [SerializeField]
+    GameObject m_Background;
+    List<GameObject> m_Paralax;
+    bool m_WorkingBackground = false;
 
     EditMode m_Mode = EditMode.Move;
     LineRenderer m_EndLine;
@@ -32,6 +37,7 @@ public class LevelEditor : MonoBehaviour
     void Start()
     {
         m_Tiles = new List<GameObject>();
+        m_Paralax = new List<GameObject>();
         m_EndLine = gameObject.AddComponent<LineRenderer>();
         ResizeLevel(16, 8);
 
@@ -45,13 +51,36 @@ public class LevelEditor : MonoBehaviour
 
     void Update()
     {
+        //This is gross and needs refactoring.
         if (Input.touchCount > 0)
         {
             if (Input.GetTouch(0).phase == TouchPhase.Began) m_PreviousTouchPosition = Input.GetTouch(0).position;
 
             if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Moved)
             {
-                if (m_Mode == EditMode.Move)
+                if (m_WorkingBackground)
+                {
+                    if (m_Mode == EditMode.Place)
+                    {
+                        if (Input.GetTouch(0).phase == TouchPhase.Began)
+                        {
+                            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                            pos.y = Mathf.Floor(pos.y) - 0.5f;
+                            pos.z = 0;
+                            m_Paralax.Add(Instantiate(m_Background, pos, Quaternion.identity) as GameObject);
+                        }
+                    }
+                    else
+                    {
+                        RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
+                        if (hitInfo)
+                        {
+                            EditorBackground back = hitInfo.transform.GetComponent<EditorBackground>();
+                            if (back) back.Click(Input.GetTouch(0).position, Input.GetTouch(0).phase == TouchPhase.Began);
+                        }
+                    }
+                }
+                else if (m_Mode == EditMode.Move)
                 {
                     Vector3 diff = Input.GetTouch(0).position - m_PreviousTouchPosition;
                     Camera.main.transform.position -= diff * m_ScrollSpeed;
@@ -73,7 +102,29 @@ public class LevelEditor : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            if (m_Mode == EditMode.Move)
+            if (m_WorkingBackground)
+            {
+                if (m_Mode == EditMode.Place)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        pos.y = Mathf.Floor(pos.y) - 0.5f;
+                        pos.z = 0;
+                        m_Paralax.Add(Instantiate(m_Background, pos, Quaternion.identity) as GameObject);
+                    }
+                }
+                else
+                {
+                    RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                    if (hitInfo)
+                    {
+                        EditorBackground back = hitInfo.transform.GetComponent<EditorBackground>();
+                        if (back) back.Click(Input.mousePosition, Input.GetMouseButtonDown(0));
+                    }
+                }
+            }
+            else if (m_Mode == EditMode.Move)
             {
                 Vector3 diff = Input.mousePosition - new Vector3(m_PreviousTouchPosition.x, m_PreviousTouchPosition.y);
                 Camera.main.transform.position -= diff * m_ScrollSpeed;
@@ -90,6 +141,59 @@ public class LevelEditor : MonoBehaviour
                 }
             }
         }
+    }
+
+    void SetEditMode(EditMode aMode)
+    {
+        m_Mode = aMode;
+    }
+
+    void SetBackground(bool aYes)
+    {
+        if (aYes)
+        {
+            foreach (GameObject t in m_Tiles)
+            {
+                ToggleObject(t, false);
+            }
+
+            foreach (GameObject b in m_Paralax)
+            {
+                ToggleObject(b, true);
+            }
+        }
+        else
+        {
+            foreach (GameObject t in m_Tiles)
+            {
+                ToggleObject(t, true);
+            }
+
+            foreach (GameObject b in m_Paralax)
+            {
+                ToggleObject(b, false);
+                b.GetComponent<EditorBackground>().SetSelected(false);
+            }
+        }
+
+        m_WorkingBackground = aYes;
+    }
+
+    void ToggleObject(GameObject aObject, bool aOn)
+    {
+        Renderer rend = aObject.GetComponent<Renderer>();
+        Collider2D hit = aObject.GetComponent<Collider2D>();
+
+        Color c = rend.material.color;
+        c.a = (aOn ? 1 : 0.25f);
+        rend.material.color = c;
+        hit.enabled = aOn;
+    }
+
+    public void RemoveBackground(GameObject aObject)
+    {
+        m_Paralax.Remove(aObject);
+        Destroy(aObject);
     }
 
     void ResizeLevel(int aWidth, int aHeight)
@@ -180,12 +284,23 @@ public class LevelEditor : MonoBehaviour
         levelData.Height = m_Height;
         levelData.EndPoint = m_EndPoint;
         levelData.Tiles = new List<EditorTile.TileState>();
+        levelData.Background = new List<Position3>();
 
         //Populate tiles
         foreach (GameObject tile in m_Tiles)
         {
             EditorTile t = tile.GetComponent<EditorTile>();
             if (t) levelData.Tiles.Add(t.GetTileState());
+        }
+
+        //Populate background
+        foreach (GameObject back in m_Paralax)
+        {
+            Position3 pos = new Position3();
+            pos.x = back.transform.position.x;
+            pos.y = back.transform.position.y;
+            pos.z = back.transform.position.z;
+            levelData.Background.Add(pos);
         }
 
         BinaryFormatter binaryFormatter = new BinaryFormatter();
@@ -252,10 +367,21 @@ public class LevelEditor : MonoBehaviour
 
             if (tile) tile.Click();
         }
-        
+
+        //Load Background
+        while (m_Paralax.Count > 0)
+        {
+            RemoveBackground(m_Paralax[0]);
+        }
+        for (int i = 0; i < levelData.Background.Count; i++)
+        {
+            m_Paralax.Add(Instantiate(m_Background, new Vector3(levelData.Background[i].x, levelData.Background[i].y, levelData.Background[i].z), Quaternion.identity) as GameObject);
+        }
+
         //Close the file stream
         loadStream.Close();
 
+        SetBackground(false);
         m_Mode = EditMode.Move;
 
         Debug.Log("Load Successful!");
@@ -291,9 +417,10 @@ public class LevelEditor : MonoBehaviour
         GUI.Label(new Rect(Screen.width - 330, Screen.height - 40, 120, 30), m_EndPoint.ToString());
 
         //Mode
-        if (GUI.Button(new Rect(10, 10, 60, 30), "Move")) m_Mode = EditMode.Move;
-        if (GUI.Button(new Rect(10, 50, 60, 30), "Place")) m_Mode = EditMode.Place;
-        if (GUI.Button(new Rect(10, 90, 60, 30), "Clear")) m_Mode = EditMode.Clear;
+        if (GUI.Button(new Rect(10, 10, 60, 30), "Move")) SetEditMode(EditMode.Move);
+        if (GUI.Button(new Rect(10, 50, 60, 30), "Place")) SetEditMode(EditMode.Place);
+        if (GUI.Button(new Rect(10, 90, 60, 30), "Clear")) SetEditMode(EditMode.Clear);
+        if (GUI.Button(new Rect(10, 130, 60, 30), "Back")) SetBackground(!m_WorkingBackground);
 
         //Save and Load
         m_FileName = GUI.TextField(new Rect(10, Screen.height - 110, 120, 20), m_FileName);
@@ -303,10 +430,19 @@ public class LevelEditor : MonoBehaviour
 }
 
 [Serializable]
+public class Position3
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+[Serializable]
 public class SaveData
 {
     public int Width;
     public int Height;
     public int EndPoint;
     public List<EditorTile.TileState> Tiles;
+    public List<Position3> Background;
 }
