d6ba5b3fc3b07afcabec50fa1b4aee1f41f722e5
Toboshi <evan.welchner@rogers.com>
2016-06-16 15:52:16 -0400

    Implemented trampoline

---
+++ ToyTale/Assets/_Project/Scripts/Tools/Trampoline.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField]
+    float m_JumpPower = 7;
+
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        if (hit.gameObject.tag != "Player") return;
+
+        float angle = Vector2.Angle(hit.contacts[0].normal, transform.up);
+        bool fromTop = 1 * Mathf.Sign(hit.rigidbody.velocity.y) * Mathf.Sign(transform.up.y) < 0;
+        if (fromTop && angle < 10 || angle > 170)
+        {
+            Vector2 vel = hit.rigidbody.velocity;
+            vel.y = m_JumpPower;
+            hit.rigidbody.velocity = vel;
+        }
+    }
+}
