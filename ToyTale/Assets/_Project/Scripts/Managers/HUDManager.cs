edb4e98419cdec2c7a6a629eb0927a87798445d4
Ameliane <Ameliane@users.noreply.github.com>
2016-06-18 22:29:30 -0400

    Fixed the tool placement in the level.

--- ToyTale/Assets/_Project/Scripts/Managers/HUDManager.cs
+++ ToyTale/Assets/_Project/Scripts/Managers/HUDManager.cs
@@ -2,54 +2,51 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class UIManager : MonoBehaviour
+public class HUDManager : MonoBehaviour
 {
+    public GameObject[] m_Tools;
+    public GameObject[] m_ToolPrefabs;
+    private Vector3[] m_ToolStartPos;
 
     // Use this for initialization
     void Start()
     {
+        m_ToolStartPos = new Vector3[m_Tools.Length];
 
+        for (int i = 0; i < m_ToolStartPos.Length; i++)
+        {
+            m_ToolStartPos[i] = m_Tools[i].transform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Drag(GameObject aTool)
     {
-
+        aTool.transform.position = Input.mousePosition;
+        //aTool.transform.position = Input.GetTouch(0).position;
     }
 
-    public void Drag(GameObject aThis)
-    {
-        aThis.transform.position = Input.mousePosition;
-    }
-
-    public void Drop()
-    {
-        Debug.Log("Drop");
-    }
-
-    public void Select()
-    {
-        Debug.Log("Select");
-    }
-
-    public void Deselect()
-    {
-        Debug.Log("Deselect");
-    }
-
-    public void Move()
-    {
-        Debug.Log("Move");
-    }
-
-    public void BeginDrag()
+    public void BeginDrag(GameObject aTool)
     {
         Debug.Log("BeginDrag");
     }
 
-    public void EndDrag()
+    public void EndDrag(GameObject aTool)
     {
+        int index = 0;
+        for (int i = 0; i < m_Tools.Length; i++)
+        {
+            if (m_Tools[i] == aTool)
+            {
+                index = i;
+            }
+        }
+
         Debug.Log("EndDrag");
+        Vector3 newPos = Vector3.zero;
+        newPos = Camera.main.ScreenToWorldPoint(aTool.transform.position);
+        newPos.z = 0;
+        GameObject.Instantiate(m_ToolPrefabs[index], newPos, Quaternion.identity);
+        aTool.transform.position = m_ToolStartPos[index];
     }
     
 }
